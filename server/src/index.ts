d3304e3a6348748f2ccb615112fee11a5f593226
Beatso <39217836+Beatso@users.noreply.github.com>
2021-07-04 19:29:42 +0100

    add express skeleton code

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,16 @@
-console.log("hello world")
+import express from "express"
+
+// express setup
+const app = express()
+app.use(express.json())
+app.get("/ping", (req, res) => res.send("pong"))
+// app.all("*", (req, res, next) => {
+// 	res.header("access-control-allow-origin", "*")
+// 	next()
+// })
+
+// ...
+
+// listen express server
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`Server running on port ${port}`))
